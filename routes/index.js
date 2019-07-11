89147450d7de77a6f4444c8d4a9c59d75388e246
Maynor Peña <38294810+PENA98@users.noreply.github.com>
2019-07-10 22:13:43 -0600

    Inicio de trabajo en las rutas

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+//express router
+const express = require('express');
+const router = express.Router();
+
+// Importar express-validator
+const { body } = require('express-validator/check');
+
+// Importar el Controller
+
+// TODO: Falta implementar el controlador
+
+module.exports = function(){
+
+
+    
+}
