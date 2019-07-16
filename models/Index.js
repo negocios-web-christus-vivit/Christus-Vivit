0a5044a500b58bfd6c0b1c85303049e8b1432981
luisdario71196 <luisdario500@gmail.com>
2019-07-15 20:00:20 -0600

    Incorporar el beforeCreate y beforeUpdate para el modelo

--- models/Index.js
+++ models/Index.js
@@ -1,6 +1,8 @@
 // Importar Sequelize
 const Sequelize = require('sequelize');
 
+// Importar Slug
+
 // Importar  la base de datos
 const db = require('../config/db');
 
@@ -16,7 +18,32 @@ const Index = db.define('index', {
     // TODO: Posiblidad de mas campos para el modelo
 
     url: Sequelize.STRING
-});
+
+},{
+    hooks: {
+        
+
+        beforeCreate(index) {
+
+            console.log('Antes de Registrar');
+            // const url = slug(index)
+
+            // TODO: REGISTRAR DATOS 
+            
+        },
+        beforeUpdate(index) {
+
+
+            // TODO: ACTUALIZAR DATOS
+            
+        }
+        
+    }
+}
+
+
+);
 
 
 // Importar el modelo para ser utilizado
+module.exports = Index;
