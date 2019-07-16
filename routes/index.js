72cd8cc7199aec30731116c2a7e744ca18220569
Maynor Peña <38294810+PENA98@users.noreply.github.com>
2019-07-15 21:48:51 -0600

    Index send funcionando

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 // Importar express-validator
-const { body } = require('express-validator/check');
+const { body } = require('express-validator');
 
 // Importar el Controller
 
 // TODO: Falta implementar el controlador
+const articulosController = require('../controllers/articuloController')
 
 module.exports = function(){
-
-
+    router.get('/', articulosController.articulosHome);
     
+    return router;
 }
